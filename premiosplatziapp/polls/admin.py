6dce26bd9af414b76b4ef20a42003a88e6e4bc78
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-06-02 08:48:05 -0400

    created views

--- premiosplatziapp/polls/admin.py
+++ premiosplatziapp/polls/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Question, Choice
 
-# Register your models here.
+admin.site.register([Question, Choice])
+
+# class ChoicesInline(admin.StackedInline):
+#     model = Choice
+#     can_delete = False
+#     verbose_name_plural = 'choices'
+
+# @admin.register(Question)
+# class QuestionAdmin(admin.ModelAdmin):
+#     inlines = (ChoicesInline,)
--- premiosplatziapp/premiosplatziapp/settings.py
+++ premiosplatziapp/premiosplatziapp/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+THIRD_APPS = [
+    #'jazzmin',
+]
+
+LOCAL_APPS = [
+    'polls.apps.PollsConfig',
+]
+
+INSTALLED_APPS = THIRD_APPS + INSTALLED_APPS  + LOCAL_APPS
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
