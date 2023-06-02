c6147b26cc8346464ef93f4122c9cd6c8aac5d42
Camila Sandoval <mariacamilasandoval@hotmail.com>
2023-06-01 22:48:23 -0400

    created model for db

--- premiosplatziapp/premiosplatziapp/settings.py
+++ premiosplatziapp/premiosplatziapp/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -77,6 +78,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        #user
+        #password
+        #sqlite3 doesnt require this ones
     }
 }
 
